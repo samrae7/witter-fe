f3cedb27b7b595607dcdee4329540c7ffe79b14a
Sam Rae <samuelrae@gmail.com>
2019-08-17 22:21:32 +0100

    create pages for all reviews + add slug to each review node

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,47 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`);
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  console.log(node.internal.type);
+  if (node.internal.type === `Review`) {
+    const { createNodeField } = actions;
+    const slug = `/film/${node.id}`;
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allReview {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  result.data.allReview.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/film.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
+    });
+  });
+};

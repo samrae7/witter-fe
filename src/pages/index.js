deef99796a1e2d15a5d311097ed603ffee1845b3
Sam Rae <samuelrae@gmail.com>
2019-08-17 22:22:17 +0100

    link to all review pages from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,49 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <p>Welcome to the Witter Feed.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <ul>
+        {data.allReview.edges.map(review => {
+          return (
+            <li>
+              <Link to={review.node.fields.slug}>{review.node.film.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+};
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export const query = graphql`
+  query IndexPageQuery {
+    allReview {
+      edges {
+        node {
+          id
+          film {
+            name
+            id
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
 
-export default IndexPage
+export default IndexPage;

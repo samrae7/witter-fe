ea939e9e2fe2f26a47b66e420f2c2f45e2232c44
Sam Rae <samuelrae@gmail.com>
2019-08-26 19:14:49 +0100

    menu styling

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,34 +1,61 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { MdMenu } from "react-icons/md";
+import { MdMenu, MdClose } from "react-icons/md";
+import { Link } from "gatsby";
 
 const Item = styled.li`
   color: white;
   border-top: white solid 1px;
   background-color: rebeccapurple;
+  padding-left: 1rem;
+`;
+
+const StyledLink = styled(props => <Link {...props} />)`
+  color: white;
+  text-decoration: none;
 `;
 
 const StyledMenu = styled.menu`
   list-style: none;
   position: absolute;
+  padding-left: 0;
   right: 0;
   width: 100px;
   margin: 0;
   width: 100%;
+  font-family: typeface-cousine, monospace;
+`;
+
+const IconContainer = styled.div`
+  padding: 4px 2px 0 2px;
+  border-left: 1px solid white;
+  background-color: rebeccapurple;
 `;
 
 const Menu = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
   return (
     <div>
-      <MdMenu
-        size={48}
-        color="white"
-        onClick={() => setMenuOpen(!isMenuOpen)}
-      />
+      <IconContainer onClick={() => setMenuOpen(!isMenuOpen)}>
+        {isMenuOpen ? (
+          <MdClose size={36} color="white" />
+        ) : (
+          <MdMenu size={36} color="white" />
+        )}
+      </IconContainer>
+
       {isMenuOpen && (
         <StyledMenu>
-          <Item>About</Item>
+          <Item>
+            <StyledLink onClick={() => setMenuOpen(!isMenuOpen)} to="/">
+              <div>Home</div>
+            </StyledLink>
+          </Item>
+          <Item>
+            <StyledLink onClick={() => setMenuOpen(!isMenuOpen)} to="/about">
+              <div>About</div>
+            </StyledLink>
+          </Item>
         </StyledMenu>
       )}
     </div>

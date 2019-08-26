6c831596e9c38be1610f3dfc275fb62cf91e03df
Sam Rae <samuelrae@gmail.com>
2019-08-26 19:13:50 +0100

    got storybook working

---
+++ src/stories/Menu.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import Menu from "../components/Menu";
+
+storiesOf("Menu", module).add("default", () => <Menu />);

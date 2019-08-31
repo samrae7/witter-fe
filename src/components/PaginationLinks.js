ed35a07d6b6ce7ac4be1e43b1cc7828c2489af2b
Sam Rae <samuelrae@gmail.com>
2019-08-31 07:16:24 +0100

    made simple version of pagination

--- src/components/PaginationLinks.js
+++ src/components/PaginationLinks.js
@@ -8,39 +8,14 @@ const PaginationContainer = styled.nav`
   padding: 0 10px;
 `;
 
-const PaginationList = styled.ul`
-  margin-top: 0;
-  display: flex;
-  justify-content: center;
-
-  a {
-    &[aria-current="true"] {
-      background-color: #333;
-      color: #fff;
-    }
-  }
-
-  .visuallyhidden {
-    border: 0;
-    clip: rect(0 0 0 0);
-    height: auto;
-    margin: 0;
-    overflow: hidden;
-    padding: 0;
-    position: absolute;
-    width: 1px;
-    white-space: nowrap;
-  }
-`;
-
-const LinkContainer = styled.div`
+const StyledLink = styled(props => <Link {...props} />)`
   display: block;
   padding: 0.5em 1em;
   border: 1px solid #999;
   border-radius: 0.2em;
   text-decoration: none;
 
-  &[aria-current="true"] {
+  &:hover {
     background-color: #333;
     color: #fff;
   }
@@ -53,63 +28,17 @@ function PaginationLinks({ currentPage, numPages }) {
     currentPage - 1 <= 1 ? "/" : `/films/${(currentPage - 1).toString()}`;
   const nextPage = `/films/${(currentPage + 1).toString()}`;
 
-  const links = [];
-  for (let i = 1; i < numPages && i <= 5; i++) {
-    links.push(
-      <li>
-        <LinkContainer>
-          <Link to={`/films/${i}`} aria-current={i === currentPage}>
-            {i}
-          </Link>
-        </LinkContainer>
-      </li>
-    );
-  }
-
-  const StyledLink = styled(props => <Link {...props} />)`
-    text-decoration: none;
-  `;
-  const prevLink = currentPage - 1 <= 1 ? null : prevPage;
-  console.log("prevLink", currentPage - 1 > 1);
-
   return (
-    // <nav>
-    //   <PaginationList>
-    //     <li>
-    //       <LinkContainer>
-    //         {currentPage > 1 ? <Link to={prevLink}>«</Link> : <span>«</span>}
-    //       </LinkContainer>
-    //     </li>
-    //     {links}
-    //     <li>
-    //       <LinkContainer>
-    //         {currentPage < numPages ? (
-    //           <Link to={nextPage}>»</Link>
-    //         ) : (
-    //           <span>«</span>
-    //         )}
-    //       </LinkContainer>
-    //     </li>
-    //   </PaginationList>
-    // </nav>
     <PaginationContainer aria-label="pagination">
       {!isFirst && (
-        <LinkContainer style={{ marginRight: `auto` }}>
-          {currentPage > 1 ? (
-            <StyledLink to={prevLink}>«</StyledLink>
-          ) : (
-            <span>«</span>
-          )}
-        </LinkContainer>
+        <StyledLink style={{ marginRight: `auto` }} to={prevPage}>
+          «
+        </StyledLink>
       )}
       {!isLast && (
-        <LinkContainer style={{ marginLeft: `auto` }}>
-          {currentPage < numPages ? (
-            <StyledLink to={nextPage}>»</StyledLink>
-          ) : (
-            <span>«</span>
-          )}
-        </LinkContainer>
+        <StyledLink style={{ marginLeft: `auto` }} to={nextPage}>
+          »
+        </StyledLink>
       )}
     </PaginationContainer>
   );

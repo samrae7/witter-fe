c361081096cb5c2c0e5c9d49a368a1706c3de80a
Sam Rae <samuelrae@gmail.com>
2019-09-08 10:26:22 +0100

    search adjustments

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { Dropdown } from "semantic-ui-react";
 import { navigate } from "gatsby";
 
+// TODO pass custom icon so that icon changes to X when open
+// const Icon = () => <i class="search icon"></i>;
+
 function SearchForm() {
   const data = useStaticQuery(graphql`
     query searchDataQuery {
-      allReview {
+      allReview(sort: { fields: film___name }) {
         edges {
           node {
             id
@@ -28,9 +31,20 @@ function SearchForm() {
     const { film, fields } = r.node;
     return {
       key: film.name,
-      text: name,
+      text: film.name,
       value: fields.slug,
     };
+  });
+
+  // I don't want the top list item to automatically show in search box
+  films.unshift({
+    key: "placeholder",
+    text: "Search here",
+    value: "",
+    active: true,
+    style: {
+      display: "none",
+    },
   });
 
   return (
